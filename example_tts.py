48fffd2b18ad4089739242ded5c59bf38525ecac
karbaj <106492912+karbaj@users.noreply.github.com>
2025-12-04 19:58:20 -0800

    Update example_tts.py

--- example_tts.py
+++ example_tts.py
@@ -1,4 +1,4 @@
-import torchaudio as ta
+import soundfile as sf
 import torch
 from chatterbox.tts import ChatterboxTTS
 from chatterbox.mtl_tts import ChatterboxMultilingualTTS
@@ -17,15 +17,15 @@ model = ChatterboxTTS.from_pretrained(device=device)
 
 text = "Ezreal and Jinx teamed up with Ahri, Yasuo, and Teemo to take down the enemy's Nexus in an epic late-game pentakill."
 wav = model.generate(text)
-ta.save("test-1.wav", wav, model.sr)
+sf.write("test.wav", wav.squeeze().cpu().numpy(), model.sr)
 
 multilingual_model = ChatterboxMultilingualTTS.from_pretrained(device=device)
 text = "Bonjour, comment ça va? Ceci est le modèle de synthèse vocale multilingue Chatterbox, il prend en charge 23 langues."
 wav = multilingual_model.generate(text, language_id="fr")
-ta.save("test-2.wav", wav, multilingual_model.sr)
+sf.write("test-2.wav", wav.squeeze().cpu().numpy(), multilingual_model.sr)
 
 
 # If you want to synthesize with a different voice, specify the audio prompt
 AUDIO_PROMPT_PATH = "YOUR_FILE.wav"
 wav = model.generate(text, audio_prompt_path=AUDIO_PROMPT_PATH)
-ta.save("test-3.wav", wav, model.sr)
+sf.write("test-3.wav", wav.squeeze().cpu().numpy(), model.sr)
